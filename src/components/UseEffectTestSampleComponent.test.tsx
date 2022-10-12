f7968959aee2d9778aac3f4f30453b5521dbdc97
yugo uchiida <y.uchiida@nirneege.com>
2022-10-13 05:17:04 +0900

    update testcases

--- src/components/UseEffectTestSampleComponent.test.tsx
+++ src/components/UseEffectTestSampleComponent.test.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import UseEffectTestSampleComponent from "./UseEffectTestSampleComponent";
+import '@testing-library/jest-dom';
 
 describe("useEffect を用いた初期処理のテスト", () => {
+	it("APIからのレスポンスが返る前は、何も描画されない", async () => {
+		render(<UseEffectTestSampleComponent />);
+
+		/* レンダリング直後は何も表示しない */
+		expect(screen.queryByTestId('userGreet')).toBeNull();
+	});
+
 	it("APIからのレスポンスが返った後に、内容が描画される", async () => {
 		render(<UseEffectTestSampleComponent />);
 
-		/* レンダリング直後は */
-		expect(screen.queryByTestId('userGreet')).toBeNull();
-
+		/* await で要素が出現するまで待機した後、評価を行う */
+		expect(await screen.findByTestId('userGreet')).toBeInTheDocument();
 	});
 });
